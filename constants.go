ebeb3d57c00400eeaa1c0ab73e95df95881321c3
smagic39 <smagic39@gmail.com>
2015-11-24 10:50:40 +0700

    add constants

--- constants.go
+++ constants.go
@@ -1,15 +1,15 @@
 package main
 
-import ("fmt")
-import ("math")
-
-const s string = "constants"
-func main(){
+import (
+	"fmt"
+)
+const s  = "constant oh shit"
+const pi = 3.14
+const d = 12/2
+func main()  {
 
 	fmt.Println(s)
-	const n = 50000000
-	const d = 3e20/n
-	fmt.Println(d)
-	fmt.Println(math.Sin(n))
+	fmt.Println(pi)
+	fmt.Print(d)
 
 }
